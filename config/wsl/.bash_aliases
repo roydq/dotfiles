433a92189aad660f2dbcfa1962e0377174a11736
Roy Quader <35521+roydq@users.noreply.github.com>
2025-05-26 10:02:05 -0400

    NvChad and remove craps

--- config/wsl/.bash_aliases
+++
@@ -1,3 +0,0 @@
-source ~/.shell/environment
-source ~/.shell/functions
-source ~/.shell/aliases
--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -36,26 +36,6 @@ done
 echo "Done."
 echo ""
 
-# Check if this is WSL. If so, link some WSL-specific dotfiles
-# Might not actually be necessary, this was with WSL 1
-if [[ $PATH == *"Windows"* ]]; then
-  # Used for Windows Subsystem for linux
-  wsl_linked_paths=(.bash_aliases .bashrc)
-
-  # Backup default .bashrc
-  if [[ ! -L $HOME/.bashrc && -f $HOME/.bashrc ]]; then
-    mv $HOME/.bashrc $HOME/.bashrc.default
-  fi
-
-  echo "WSL detected, linking bash config..."
-  for item in "${wsl_linked_paths[@]}"
-  do
-    create_link $config_path/wsl/$item $HOME/$item
-  done
-  echo "Done"
-  echo ""
-fi
-
 # Install tmux plugin manager
 echo "Cloning tmux plugin manager..."
 tpm_dir=$HOME/.tmux/plugins/tpm
